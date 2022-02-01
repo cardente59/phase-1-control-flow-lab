4a615194a8f6462670cb42952922c2422f6f4798
Alan <cardente59@gmail.com>
2022-02-01 18:50:48 -0500

    third

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride=199){
+  if(ride <= 400){
+ return 'This one is on me!'
+ } 
+  else if (ride >= 2500){
+   return "No can do."
+} 
+ else if(ride >= 2000){
+   return "I will gladly take your thirty bucks."
+ }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(city){
+   return city === 'NYC'?'Ok, sounds good.': 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch(tipAmount){
+    case "generous":
+   let reponse = "Thank you so much."
+    return reponse 
+    case "not as generous":
+      return 'Thank you.'
+      default : 
+      return 'Bye.'
+  }
 }
